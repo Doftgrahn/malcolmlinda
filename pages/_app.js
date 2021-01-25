889068e3793e232cef96de08e1b06fd32501af42
Simon grahn <simon.grahn@gmail.com>
2021-01-25 20:53:56 +0100

    k

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import { ReactQueryDevtools } from 'react-query-devtools';
-import { Html, Head, Main, NextScript } from 'next/document';
 
 import { QueryCache, ReactQueryCacheProvider } from 'react-query';
 
@@ -23,56 +22,6 @@ function MyApp({ Component, pageProps }) {
 
     return (
         <ReactQueryCacheProvider queryCache={queryCache}>
-            <Head>
-                {/* PWA primary color */}
-                <meta name='theme-color' content={theme.palette.primary.main} />
-                <title>Linda och Malcom, 24 Juli 2021</title>
-                <meta
-                    name='description'
-                    content='Här kommer du hitta information som om hur, när, var Malcolm får sin Linda eller om Linda får sin Malcolm.'
-                />
-                <meta
-                    property='og:title'
-                    content='Linda och Malcom, 24 Juli 2021'
-                />
-                <meta
-                    property='og:description'
-                    content='Här kommer du hitta information som om hur, när, var Malcolm får sin Linda eller om Linda får sin Malcolm.'
-                />
-                <meta property='og:image' content='/images/share.png' />
-                <meta
-                    property='og:url'
-                    content='https://www.hargantsson2021.se'
-                />
-
-                <link rel='stylesheet' href='/fonts/style.css' />
-                <link rel='preconnect' href='https://fonts.gstatic.com' />
-                <link
-                    href='https://fonts.googleapis.com/css2?family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&display=swap'
-                    rel='stylesheet'
-                />
-
-                <link
-                    rel='apple-touch-icon'
-                    sizes='180x180'
-                    href='/images/favicon/apple-touch-icon.png'
-                />
-                <link
-                    rel='icon'
-                    type='image/png'
-                    sizes='32x32'
-                    href='/images/favicon/favicon-32x32.png'
-                />
-                <link
-                    rel='icon'
-                    type='image/png'
-                    sizes='16x16'
-                    href='/images/favicon/favicon-16x16.png'
-                />
-                <link rel='manifest' href='/images/favicon/site.webmanifest' />
-                <meta name='msapplication-TileColor' content='#da532c' />
-                <meta name='theme-color' content='#ffffff' />
-            </Head>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <Header />
--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,61 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang='en'>
+                <Head>
+                    {/* PWA primary color */}
+                    <meta
+                        name='theme-color'
+                        content={theme.palette.primary.main}
+                    />
+                    <meta
+                        name='description'
+                        content='Här kommer du hitta information som om hur, när, var Malcolm får sin Linda eller om Linda får sin Malcolm.'
+                    />
+                    <meta
+                        property='og:title'
+                        content='Linda och Malcom, 24 Juli 2021'
+                    />
+                    <meta
+                        property='og:description'
+                        content='Här kommer du hitta information som om hur, när, var Malcolm får sin Linda eller om Linda får sin Malcolm.'
+                    />
+                    <meta property='og:image' content='/images/share.png' />
+                    <meta
+                        property='og:url'
+                        content='https://www.hargantsson2021.se'
+                    />
+
+                    <link rel='stylesheet' href='/fonts/style.css' />
+                    <link rel='preconnect' href='https://fonts.gstatic.com' />
+                    <link
+                        href='https://fonts.googleapis.com/css2?family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&display=swap'
+                        rel='stylesheet'
+                    />
+
+                    <link
+                        rel='apple-touch-icon'
+                        sizes='180x180'
+                        href='images/favicon/apple-touch-icon.png'
+                    />
+                    <link
+                        rel='icon'
+                        type='image/png'
+                        sizes='32x32'
+                        href='images/favicon/favicon-32x32.png'
+                    />
+                    <link
+                        rel='icon'
+                        type='image/png'
+                        sizes='16x16'
+                        href='images/favicon/favicon-16x16.png'
+                    />
+                    <link
+                        rel='manifest'
+                        href='images/favicon/site.webmanifest'
+                    />
+                    <meta name='msapplication-TileColor' content='#da532c' />
+                    <meta name='theme-color' content='#ffffff' />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
